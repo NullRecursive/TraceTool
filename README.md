e83b36eb6a163dcc091030d05480c62aba50a3d7
brunowall <brunowallison.fernandes@gmail.com>
2017-04-14 15:23:09 -0300

    alteracoes feitas

---
+++ README.md
@@ -0,0 +1,2 @@
+#Trace Tool 
+
--- tracetool.c
+++ tracetool.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pcap.h>
+#include <string.h>
+
+#define TRUE 1
+#define FALSE 0
 
 void print_devices(int ret,  pcap_if_t**iface, char * errbuff){
 
@@ -17,18 +21,26 @@ void print_devices(int ret,  pcap_if_t**iface, char * errbuff){
 			(it->description == NULL) ? printf("%s\n\n", ""): printf("%s\n\n", it->description);
 		}
 	}
-	
 }
 
 
 
+int main (int argc, char *argv[]){
 
-int main (int argc, char ** argv){
 
-	pcap_if_t ** iface;
-	char *  errbuff;
-	int ret = pcap_findalldevs(iface,errbuff);
-	print_devices(ret,iface,errbuff);
+	if(argc <= 1){
+		return 1;
+	}
 
+	for(int i = 1; (*(argv+i)) != NULL; i++){
+
+		if(!strcmp( (*(argv+i)),"-li")){
+			pcap_if_t ** iface;
+			char *  errbuff;
+			int ret = pcap_findalldevs(iface,errbuff);
+			print_devices(ret,iface,errbuff);
+		}
+
+	}
 
 }
